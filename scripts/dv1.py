95704fceaa517e50658f8d16a7a1e97f2607ead4
HugoCasa <hugo@casademont.ch>
2019-11-29 19:40:01 +0100

    models and adaptive

--- scripts/dv1.py
+++ scripts/dv1.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,89 +5,45 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
 
+# all data
+prec = pd.read_csv('../data/MH25_vaisalawxt520prec_2017.csv')
+prec['time'] = pd.to_datetime(prec['time'])
+wind = pd.read_csv('../data/MH25_vaisalawxt520windpth_2017.csv')
+wind['time'] = pd.to_datetime(wind['time'])
+temp = pd.read_csv('../data/MH30_temperature_rock_2017.csv')
+temp['time'] = pd.to_datetime(temp['time'])
+radio = pd.read_csv('../data/MH15_radiometer__conv_2017.csv')
+radio['time'] = pd.to_datetime(radio['time'])
 
-prec = pd.read_csv('data/MH25_vaisalawxt520prec_2017.csv')
-wind = pd.read_csv('data/MH25_vaisalawxt520windpth_2017.csv')
-temp = pd.read_csv('data/MH30_temperature_rock_2017.csv')
-radio = pd.read_csv('data/MH15_radiometer__conv_2017.csv')
+# join all data by time
+temp0 = pd.merge(left=temp, right=prec, left_on='time', right_on='time')
+temp0 = pd.merge(left=temp0, right=wind, left_on='time', right_on='time')
+temp0 = pd.merge(left=temp0, right=radio, left_on='time', right_on='time')
 
-temp_inner = pd.merge(left= temp, right=prec, left_on='time',right_on='time')
-temp_inner = pd.merge(left= temp_inner, right=wind, left_on='time',right_on='time')
-temp_inner = pd.merge(left=temp_inner, right=radio, left_on='time',right_on='time')
+# difference between ground and deepest temperature --> objective
+diff = temp0['temperature_100cm [°C]'] - temp0['temperature_5cm [°C]']
+temp0.insert(0, 'delta_t', diff)
 
+# remove useless columns
+del temp0['position []_x']
+del temp0['position []_y']
 
-diff = temp_inner['temperature_100cm [°C]'] - temp_inner['temperature_5cm [°C]']
+# show correlation (within same time period)
+print(abs(temp0.corr()['delta_t']).sort_values(ascending=False))
 
+# clean column names and remove inf/nas
+temp0.columns = temp0.columns.str.replace("[", "_")
+temp0.columns = temp0.columns.str.replace("]", "_")
+temp0 = temp0.fillna(0)
+temp0.replace([np.inf, -np.inf], 0)
 
-del temp_inner['time']
-
-
-
-del temp_inner['position []_x']
-
-
-
-diff.shape
-
-
-
-del temp_inner['delta_t']
-temp_inner.insert(0,'delta_t',diff)
-
-
-
-
-abs(temp_inner.corr()['delta_t']).sort_values(ascending=False)
-
-
-
-temp_inner.reset_index()
-
-
-
-n = temp_inner.shape[0]
-train_ind = int(np.round(0.7 * n))
-print(train_ind)
-
-
-
-temp_inner.columns = temp_inner.columns.str.replace("[", "_")
-temp_inner.columns = temp_inner.columns.str.replace("]", "_")
-
-
-temp_inner = temp_inner.fillna(0)
-temp_inner.replace([np.inf, -np.inf], 0)
-
-
-
-X, y = temp_inner.iloc[:,1:],temp_inner.iloc[:,0]
-
-
-
-
+# create test and train samples
+X, y = temp0.iloc[:, 1:], temp0.iloc[:, 0]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
 
-
-
-
-X_train.shape
-
-
-
-model = sm.OLS(y_train,X_train).fit()
-
-
-
-
+# train model and predict
+model = sm.OLS(y_train, X_train).fit()
 preds = model.predict(X_test)
 
-
-
-
 model.summary()
-
 plt.plot(preds, y_test)
-
-
-
-
